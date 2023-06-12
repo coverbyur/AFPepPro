b6e30b3db3e2d989b1970ea19e765344e3bda31a
coverbyur <136386512+coverbyur@users.noreply.github.com>
2023-06-12 17:01:29 -0400

    Add files via upload

---
+++ AFPeptidePro/__init__.py
@@ -0,0 +1,11 @@
+'''AFPepPro is a module containing various tools to investigate peptide self-aggregation'''
+from .AFPepPro import start
+from .AFPepPro import read_param
+from .AFPepPro import main_handler
+from .AFPepPro import read_aa_tables_pasta
+from .AFPepPro import PeptideContainer
+from .AFPepPro import gen_charge_seq
+from .AFPepPro import gen_charge_seq_list
+from .AFPepPro import compare_sub_seq
+from .AFPepPro import make_pep_from_chsq
+from .AFPepPro import pasta
